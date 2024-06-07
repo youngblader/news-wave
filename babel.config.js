b6e4ebb250add3e11a02613ff279135aa83743cb
Eugene <56676493+youngblader@users.noreply.github.com>
2024-06-07 19:06:17 +0300

    chore: add deps

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@screens': ['src/screens'],
+          '@hooks': ['src/hooks'],
+          '@constants': ['src/constants'],
+          '@store': ['src/store'],
+          '@navigation': ['src/navigation'],
+          '@models': ['src/models'],
+          '@styles': ['src/styles'],
+        },
+      },
+    ],
+  ],
 };
