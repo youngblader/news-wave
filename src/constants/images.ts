eea54b226908bd5ef89f0d484e0c0050c83ba2af
Eugene <56676493+youngblader@users.noreply.github.com>
2024-06-12 14:31:07 +0300

    chore: add image

---
+++ src/constants/images.ts
@@ -0,0 +1,12 @@
+export const IMAGES = {
+  bookmarks: require('../assets/images/bookmark.png'),
+  bookmarksSelected: require('../assets/images/bookmark-selected.png'),
+  chevron: require('../assets/images/chevron.png'),
+  home: require('../assets/images/home.png'),
+  logo: require('../assets/images/logo.png'),
+  menu: require('../assets/images/menu.png'),
+  search: require('../assets/images/search.png'),
+  searchInput: require('../assets/images/search-input.png'),
+  share: require('../assets/images/share.png'),
+  plug: require('../assets/images/plug.png'),
+};
