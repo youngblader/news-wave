92a04c34abd80eefb14de9362e317f1144e4e75b
Eugene <56676493+youngblader@users.noreply.github.com>
2024-06-15 14:59:33 +0300

    feat: add service

---
+++ src/services/ArticlesService.ts
@@ -0,0 +1,40 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {ArticleResponse} from '../models/ArticleResponse';
+import {ArticleCategory} from '../models/ArticleCategory';
+
+const KEY = 'YOUR KEY';
+
+export const articlesAPI = createApi({
+  reducerPath: 'articlesAPI',
+  baseQuery: fetchBaseQuery({baseUrl: 'https://newsdata.io/api/1'}),
+  tagTypes: ['Articles'],
+  endpoints: build => ({
+    fetchAllArticles: build.query<ArticleResponse, ArticleCategory>({
+      query: (category: ArticleCategory = ArticleCategory.business) => ({
+        url: '/latest',
+        method: 'GET',
+        params: {
+          apikey: KEY,
+          country: 'eg, fr, us',
+          category: category,
+          language: 'en',
+          image: '1',
+          domain: 'nytimes, bbc',
+        },
+      }),
+    }),
+    fetchSearchArticle: build.query<ArticleResponse, string>({
+      query: (text: string) => ({
+        url: '/latest',
+        method: 'GET',
+        params: {
+          apikey: KEY,
+          country: 'eg, fr, us',
+          q: text,
+          language: 'en',
+          image: '1',
+        },
+      }),
+    }),
+  }),
+});
