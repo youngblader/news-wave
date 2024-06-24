88d75a35e858594639f64b1f28e2d1008819fcec
Eugene <56676493+youngblader@users.noreply.github.com>
2024-06-24 23:27:40 +0300

    fix: fix types hook

--- src/hooks/useRoute/useRoute.ts
+++ src/hooks/useRoute/useRoute.ts
@@ -1,6 +1,6 @@
 import {RouteProp, useRoute as useAppRoute} from '@react-navigation/native';
-import {NavigationScreens} from '../../navigation/types';
+import {AppStackParamsList} from '../../navigation/types';
 
-export const useRoute = (key: keyof NavigationScreens) => {
-  return useAppRoute<RouteProp<NavigationScreens, typeof key>>();
+export const useRoute = <T extends keyof AppStackParamsList>(_: T) => {
+  return useAppRoute<RouteProp<AppStackParamsList, T>>();
 };
